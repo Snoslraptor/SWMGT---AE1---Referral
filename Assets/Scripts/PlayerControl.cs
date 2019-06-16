9aeaa0f7c40455d4bd0ada6f995ab33511e08711
Snoslraptor <33257135+Snoslraptor@users.noreply.github.com>
2019-06-16 22:13:04 +0100

    First Log
    Planning Phase and Technical Design Phases complete
    Level Tiling complete
    Basic side to side Movement (issue)
    
    This is the first time I'm pushing. I don't know how to provide evidence that the planning is done at this time, but it is. This includes; hack'n'plan gannt chart, work breakdown UML class diagrams and a state diagram.
    
    At this point the map is tiled and there is basic side to side movement on the player.
    I'm having an issue where the seems to be invisible wall where there shouldn't be. At the moment I don't know if this is a collision issue or a script issue.
    
    Currently on track with plan. Plan to implement Jumping and interaction with collectible. Will also try to solve the horizontal movement issue.

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    //movement
+    private Rigidbody2D my_rigid;
+    public float speed;
+    private bool moving;
+    private bool right;
+    //jumping
+    private bool Jumping;
+    public float jumpHeight;
+    private float airtime;
+    //ground detection
+    public Collider2D collision;
+    
+
+
+    private void Start()
+    {
+        my_rigid = GetComponent<Rigidbody2D>();
+        moving = false;
+        Jumping = false;
+    }
+
+    private void Update()
+    {
+        //--------------------------HORIZONTAL MOVEMENT---------------//
+        if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.A))
+        {
+            moving = true;
+
+            if (Input.GetKeyDown(KeyCode.D))
+                right = true;
+            else
+                right = false;
+        }
+
+        if (Input.GetKeyUp(KeyCode.D) || Input.GetKeyUp(KeyCode.A))
+            moving = false;
+
+        //-----------------------------JUMPING----------------------//
+        //if(Input.GetKeyDown(KeyCode.S))
+
+
+
+    }
+
+    private void FixedUpdate()
+    {
+        if (moving)
+        {
+            if (right)
+                my_rigid.velocity = new Vector2(speed * Time.deltaTime, my_rigid.velocity.y);
+            else
+                my_rigid.velocity = new Vector2(-speed * Time.deltaTime, my_rigid.velocity.y);
+
+        }
+        else
+            my_rigid.velocity = new Vector2(0, my_rigid.velocity.y);
+    }
+
+}
