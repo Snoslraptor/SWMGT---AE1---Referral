594ca4d535b95868fe6c8b5584f9fc3cef81b313
Snoslraptor <33257135+Snoslraptor@users.noreply.github.com>
2019-06-17 20:34:03 +0100

    Jumping + player/ collectable interaction
    Jumping and player/ collectable interaction implemented.
    
    Still have the problem of invisible walls, can jump to get out of them though
    
    jumping bug: if the player is holding down the jump button whilst landing on the ground, the jump button needs to be pressed twice before the player can jump again.
    
    Tomorrows plan is to place HUD within game and to implement collectable/ HUD interaction

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -11,10 +11,11 @@ public class PlayerControl : MonoBehaviour
     private bool right;
     //jumping
     private bool Jumping;
+    private bool falling;
     public float jumpHeight;
     private float airtime;
     //ground detection
-    public Collider2D collision;
+    public Collider2D collider2D;
     
 
 
@@ -23,6 +24,7 @@ public class PlayerControl : MonoBehaviour
         my_rigid = GetComponent<Rigidbody2D>();
         moving = false;
         Jumping = false;
+        airtime = jumpHeight;
     }
 
     private void Update()
@@ -42,10 +44,40 @@ public class PlayerControl : MonoBehaviour
             moving = false;
 
         //-----------------------------JUMPING----------------------//
-        //if(Input.GetKeyDown(KeyCode.S))
+        if (Input.GetKeyDown(KeyCode.W) && !Jumping && !falling)
+        {
+            Jumping = true;
+            //allow for passing through floor
+            if(collider2D.enabled == true)
+                collider2D.enabled = !collider2D.enabled;
+        }
 
+        //limit the amount time spent gaining altitude
+        if (Jumping)
+        {
+            airtime -= Time.deltaTime;
+            if (airtime <= 0)
+            {
+                Jumping = false;
+                //stop passing through floor
+                if (collider2D.enabled == false)
+                    collider2D.enabled = !collider2D.enabled;
+                falling = true;
+            }
+        }
 
-
+        //stop jumping, reset jumping time, turn collider back to normal
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            Jumping = false;
+            airtime = jumpHeight;
+            if (collider2D.enabled == false)
+                collider2D.enabled = !collider2D.enabled;
+            if (falling == true)
+                falling = false;
+            else
+                falling = true;
+        }
     }
 
     private void FixedUpdate()
@@ -58,8 +90,17 @@ public class PlayerControl : MonoBehaviour
                 my_rigid.velocity = new Vector2(-speed * Time.deltaTime, my_rigid.velocity.y);
 
         }
-        else
+        else if (!moving)
             my_rigid.velocity = new Vector2(0, my_rigid.velocity.y);
+
+        if (Jumping)
+            my_rigid.velocity = new Vector2(my_rigid.velocity.x, speed * Time.deltaTime);
     }
 
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("You have collided with: " + collision);
+        falling = false;
+    }
 }
